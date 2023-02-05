c43043200b4ef9c2865c3d3c32711bd3c33a49df
Caleb <calebrod1@yahoo.com>
2023-02-04 18:03:47 -0600

    finished base header! now working on my about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion';
 
 import './About.scss';
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'>
+      I Know That
+      <span>Good design</span>
+      means
+      <span>Good business</span>
+    </h2>
+
+    <div></div>
+    </>
   )
 }
 
