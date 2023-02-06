b4acb916aa4a7175fb3b660dd089cb8c640bb843
Caleb <calebrod1@yahoo.com>
2023-02-05 23:43:16 -0600

    fixed form

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,43 +1,14 @@
-import { React, useState } from "react";
+import { React } from "react";
 
 import { images } from "../../constants";
 import { AppWrap, MotionWrap } from "../../wrapper";
-import { client } from "../../client";
 import "./Footer.scss";
 
 const Footer = () => {
 
-  const [formData, setFormData] = useState({name: '', email: '', message: ''});
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const { name, email, message } = formData;
-
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-
-    setFormData({ ...formData, [name]: value});
-  }
-
-  const HandleSubmit = () => {
-    setLoading(true);
-
-    const contact = {
-      _type: 'contact',
-      name: name,
-      email: email,
-      message: message
-    }
-
-    client.create(contact).then(() => {
-      setLoading(false);
-      setIsFormSubmitted(true);
-    })
-  }
-
   return (
     <>
-      <h2 className="head-text">Contact me!</h2>
+      <h2 className="head-text">Contact me</h2>
 
       <div className="app__footer-cards">
         <div className="app__footer-card">
@@ -55,52 +26,9 @@ const Footer = () => {
         </div>
       </div>
 
-      {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
-          <div className="app__flex">
-
-            <input
-              className="p-text"
-              type="text"
-              placeholder="Your Name"
-              name="name"
-              value={name}
-              onChange={handleChangeInput}
-            />
-          </div>
-
-          <div className="app__flex">
-            <input
-              className="p-text"
-              type="text"
-              placeholder="Your Email"
-              name="email"
-              value={email}
-              onChange={handleChangeInput}
-            />
-          </div>
-
-          <div>
-            <textarea
-              className="p-text"
-              type="text"
-              placeholder="Your Message"
-              value={message}
-              name="message"
-              onChange={handleChangeInput}
-            />
-          </div>
-
-          <button type="button" className="p-text" onClick={HandleSubmit}>
-            {loading ? "Sending" : "Send Message"}
-          </button>
-
-        </div>
-      ) : (
         <div>
           <h3 className="head-text">Thank you for getting in touch!</h3>
         </div>
-      )}
     </>
   );
 };
@@ -109,5 +37,5 @@ const Footer = () => {
 export default AppWrap(
   MotionWrap(Footer, "app__footer"),
   "contact",
-  "app__whitebg"
+  "app__primarybg"
 );
