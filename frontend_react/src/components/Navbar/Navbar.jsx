badce17b7ac87c5de1db801d8c153c5e68aa9b01
Caleb <calebrod1@yahoo.com>
2023-02-05 22:58:15 -0600

    bug fixes

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -10,11 +10,12 @@ const Navbar = () => {
 
   return (
     <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        <a href="#home">
+      <a href="#home">
+        <div className="app__navbar-logo">
           <img src={images.logo} alt="logo" />
-        </a>
-      </div>
+        </div>
+      </a>
+
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,15 +5,23 @@ import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <AiFillGithub />
-      </div>
-      <div>
-        <AiFillLinkedin />
-      </div>
-      <div>
-        <AiFillMail />
-      </div>
+      <a href="https://github.com/CalebRdgz/">
+        <div>
+          <AiFillGithub />
+        </div>
+      </a>
+
+      <a href="https://www.linkedin.com/in/calebrdgz/">
+        <div>
+          <AiFillLinkedin />
+        </div>
+      </a>
+      
+      <a href="mailto:calebrodgz@gmail.com">
+        <div>
+          <AiFillMail />
+        </div>
+      </a>
     </div>
   );
 }
